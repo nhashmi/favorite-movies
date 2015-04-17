0537ecf094fc7b23cd31e60382368f2117cd87a1
tsetoung <jtmoua@yahoo.com>
2015-04-17 16:12:10 -0400

    after lunch stuff

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,34 +5,31 @@ class FavoritesController < ApplicationController
 
   # another devise helper method that retrieves the user object that has been authenticated
   def index
+    @user = current_user
     @favorites = current_user.favorites
   end
 
-  def show
-  end
-
   def new
-  end
-
-  def edit
+    @user = current_user
+    @favorite = current_user.favorites.new
   end
 
   def create
-    @favorite = current_user.favorites.new(favorite_params)
-  end
-
-  def update
+    @user = current_user
+    @favorite_new = Favorite.new
+    movie_info = @favorite_new.get_movie(params[:movie])
+    movie = @user.movies.create(
+          title: movie_info["Title"],
+          year: movie_info["Year"],
+          poster_url: movie_info["Poster"]
+    )
+    @favorite = movie.favorites.new
+    @favorite.save
+    redirect_to '/index'
   end
 
   def destroy
   end
-  
-#   private
-#   def favorite_params
-#     params.require(:user).permit(:body)
-#   end
-# end
-
 
 
 end
